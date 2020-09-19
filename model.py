167007f9aed0503f02fa10e763b85bb53f79dc0e
sickotra <sickotra@github.com>
2020-09-19 15:19:47 +0100

    Object Orientated Model
    Created a new module with a class for the agents and adapted code into an object orientated ABM

--- model.py
+++ model.py
@@ -3,23 +3,27 @@
 Created on Tue Sep 15 14:40:21 2020
 
 @author: shiva
-This file contains an Agent class definition an some code to execute  NEED TO CHNAGE THIS!
+
+This file contains an agent based model
+
 """
 
 import random  #for random number generating
 import operator #for extrating 2nd element of agents list
 import matplotlib.pyplot #for plotting agents locations
 import time #to see how long dist_between function takes to run
+import agentframework #import the created module/class called Agent
 
-random.seed(0) #'fix' the random numbers so outputs stay constant
-               #can change the seed arg anytime
+#'fix' the random numbers so outputs stay constant, can change the seed arg
+random.seed(0) 
 
 def distance_between(a, b):
     """
     Calculate the distance between a and b using Pythagoras' and return it.
      
     Take arguements a and b that are the 1st dimension of the agents list.
-    Pick out elements in the 2nd dimension of agents list to calculate dist.
+    Pick out elements in the 2nd dimension of agents list, 
+    using the Agents Class in agentframework module, to calculate dist.
  
     Parameters
     ----------
@@ -33,10 +37,11 @@ def distance_between(a, b):
     floating point number
         
     """
-    
-    y_diff = (a[0] - b[0])**2 #local variables
-    x_diff = (a[1] - b[1])**2
-    return (y_diff + x_diff)**0.5
+    #create some local variables 
+    y_diff = (a.y - b.y)**2 #difference between y coord of agents 
+    x_diff = (a.x - b.x)**2 #difference between x coord of agents
+    return (y_diff + x_diff)**0.5 
+
 
 
 num_of_agents = 10    #sets the no of agents
@@ -44,50 +49,64 @@ num_of_iterations = 100  #sets number of steps in the random walk
 
 agents = []     #Creates empty list to add sets of coords
 
-#For Loop generating random coords between 0-99, for every agent
-# then adding it to the agents list
-for i in range (num_of_agents):
-    agents.append ([random.randint(0, 99), random.randint(0, 99)])   
-print("Initial agents:")
-print (agents)
+
+
+'''#testing for 1 agent
+
+agent_1 = agentframework.Agent() # WHAT WE WANT TO GET WORKING
+#For the 1st agent, go to module agentframework and look in Agent Class. 
+#agent_1 is an instance of the class Agent, ie. instantiate
+#'agent_1' is what is passed in as 'self' 
+ 
+#to test if it works, agent_1 is now acting as an object, can use . to call any
+#functions inside the Class. 
+print (agent_1.y, agent_1.x)   #will print the INITIAL 1st agent
+                                                    
+agent_1.move()     #calling the move func from class Agent
+print (agent_1.y, agent_1.x)   #will print cood of 1st agent after 100 moves
+
+#end of testing'''
+
+
+print("Initialising agents--") 
+#Generating random coords using Agent Class, for every agent
+# then adding it to the agents list previously created
+for i in range (num_of_agents): 
+    agents.append (agentframework.Agent())      
+    #arg not needed, the init method will initialise the agents using self
+print ("Initial agents:")
+print (agents)  #prints list of all initial agents 
                         
-# Change y and x for all agents based on random numbers/ random walk 1 step
-#agents[i][0] is the y coord of each agent/element in 2D list
-#agents[i][1] is the x coord of each agent/element in 2D list
-for j in range (num_of_iterations):  #moves coords num_of_iteration times
-    
-    for i in range (num_of_agents): #steps by 1 every agents y & x coord
-        if random.random() < 0.5:    #random no between 0-1 to determine step
-            agents[i][0] = (agents[i][0] + 1) % 100 #using torus boundary
-        else: 
-            agents[i][0] = (agents[i][0] - 1) % 100
-   
-        if random.random() < 0.5:
-            agents[i][1] = (agents[i][1] + 1) % 100
-        else: 
-            agents[i][1] = (agents[i][1] - 1) % 100
+
+print ("Moving agents --")
+# Change y and x for all agents using random walk
+for j in range (num_of_iterations):   #moves the coords num_of_iteration times
+
+    #Using the move func in Agents class on every coord set in the agents list
+    for i in range (num_of_agents): 
+        agents[i].move()                       
 print("Agents after moving", num_of_iterations, "times:")
-print (agents) # 2D list of all the agents/coord sets
+print (agents) # 2D list of all the agents/coord sets after stepping
 
 
-print ("Max agent in y direction:", max (agents))   #gives max in the y direc
+#find max of objects in agents list by getting x, y attributes of the instances //I THINK?!
+max_y = max (agents, key = operator.attrgetter('y')) #gives max in the y direc
+max_x = max (agents, key = operator.attrgetter('x')) #gives max in the x direc
+print ("Max agent in y direction:", max_y)   
+print ("Max agent in x direction:", max_x)   
 
-#looks in operator func for max of 2nd element in the 2D list, max in x direc
-max_x = max (agents, key = operator.itemgetter(1))   
-print ("Max agent in x direction:", max_x)
 
-
-
+#finding distances between the agents
 #initially setting to None so that max/min dist func can compare
 max_between = None 
 min_between = None 
 
 start = time.perf_counter() #start clock to assess efficiency 
 
-##outer loop starts by fixing 1st agent stepping by 1 to last agent
+#outer loop starts by fixing 1st agent stepping by 1 to last agent
 for m in range (0, num_of_agents, 1):    
     
-    #To optimise, run through only lower triangle of distances matrix,
+    #To optimise time, run through only lower triangle of distances matrix,
     #where rows & cols are the agent, removing repeated combos and dists 
     #between an agent and itself
     for k in range (m + 1, num_of_agents, 1): 
@@ -110,8 +129,8 @@ for m in range (0, num_of_agents, 1):
 
 end = time.perf_counter() #end the timer for the calculating distances loops
 print ("time = " + str (end - start))
-print("Max distance between agents", max_between)
-print("Min disctance between agents", min_between)
+print ("Max distance between agents", max_between)
+print ("Min disctance between agents", min_between)
 
 
 
@@ -121,8 +140,10 @@ matplotlib.pyplot.xlim (0, 100)
 
 #plotting all points on a scatter graph, correct way ie x, y not y, x
 for i in range (num_of_agents):
-    matplotlib.pyplot.scatter (agents[i][1], agents[i][0])
+    #get ith obj from agents list, using Agents Class to specify x, y coords
+    matplotlib.pyplot.scatter (agents[i].x, agents[i].y) 
     
 
-matplotlib.pyplot.scatter (max_x[1], max_x[0], color='red') #max x red overlay
+matplotlib.pyplot.scatter (max_x.x, max_x.y, color='red') #max x red overlay
+matplotlib.pyplot.scatter (max_y.x, max_y.y, color='blue')#max y green overlay
 matplotlib.pyplot.show()  #displays scatter plot of agents
